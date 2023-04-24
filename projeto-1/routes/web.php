11b250538e6d761b00f3cd9c5cec426cf7e9b551
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-04-23 22:00:31 -0300

    .

--- projeto-1/routes/web.php
+++ projeto-1/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -105,6 +106,9 @@ Route::get('/tailwind', function () {
 });
 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
 
 
 /** Comando do php artisan
